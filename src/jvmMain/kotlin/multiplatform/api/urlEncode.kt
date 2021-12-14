346c7344a0c2c7d80c464d41001777ae8f09d694
Tom Warner <tomwamt@gmail.com>
2021-12-14 15:07:12 -0700

    v0.6.5 Fix urlEncode on spaces in JVM

--- src/jvmMain/kotlin/multiplatform/api/urlEncode.kt
+++ src/jvmMain/kotlin/multiplatform/api/urlEncode.kt
@@ -3,5 +3,5 @@ package multiplatform.api
 import java.net.URLEncoder
 
 internal actual fun String.urlEncode(): String {
-    return URLEncoder.encode(this, Charsets.UTF_8.name())
+    return URLEncoder.encode(this, Charsets.UTF_8.name()).replace("+", "%20")
 }
